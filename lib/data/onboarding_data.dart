10ce056186554df57c824097a15c41542911c651
SalahHassan202 <salahhassan66190@gmail.com>
2026-03-28 01:24:45 +0200

    added onboarding data file

---
+++ lib/data/onboarding_data.dart
@@ -0,0 +1,19 @@
+import 'package:cashy_app/models/onboarding_model.dart';
+
+List<OnboardingModel> onboardingList = [
+  OnboardingModel(
+    image: "assets/images/onboarding/onboarding_1.png",
+    title: "Manage Your Money Easily",
+    description: "Track your daily income and expenses easily",
+  ),
+  OnboardingModel(
+    image: "assets/images/onboarding/onboarding_2.png",
+    title: "Smart Insights",
+    description: "Understand your spending with charts",
+  ),
+  OnboardingModel(
+    image: "assets/images/onboarding/onboarding_3.png",
+    title: "Your Style",
+    description: "Switch between dark and light mode",
+  ),
+];
