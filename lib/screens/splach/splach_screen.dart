f3ff857fac9ae1a2648469f96a1cc9781ea00fbd
SalahHassan202 <salahhassan66190@gmail.com>
2026-03-28 02:02:54 +0200

    feat: implement splash screen with onboarding check logic

--- lib/screens/splach/splach_screen.dart
+++ lib/screens/splach/splach_screen.dart
@@ -1,8 +1,9 @@
 import 'dart:async';
 import 'package:cashy_app/colors/app_colors.dart';
 import 'package:cashy_app/screens/home/home_screen.dart';
+import 'package:cashy_app/screens/onboarding/onboarding_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:gap/gap.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -11,44 +12,34 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  late Animation<double> _fadeAnimation;
-  late Animation<double> _scaleAnimation;
+class _SplashScreenState extends State<SplashScreen> {
+  double opacity = 0;
 
   @override
   void initState() {
     super.initState();
 
-    _controller = AnimationController(
-      vsync: this,
-      duration: const Duration(seconds: 2),
-    );
-
-    _fadeAnimation = Tween<double>(begin: 0, end: 1).animate(_controller);
-
-    _scaleAnimation = Tween<double>(
-      begin: 0.8,
-      end: 1,
-    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeOut));
-
-    _controller.forward();
-
-    Timer(const Duration(seconds: 3), () {
-      if (mounted) {
-        Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(builder: (context) => const HomeScreen()),
-        );
-      }
+    // animation بسيطة
+    Future.delayed(const Duration(milliseconds: 500), () {
+      setState(() {
+        opacity = 1;
+      });
     });
-  }
 
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
+    // navigation logic
+    Future.delayed(const Duration(seconds: 3), () async {
+      final prefs = await SharedPreferences.getInstance();
+      bool seen = prefs.getBool('seenOnboarding') ?? false;
+
+      if (!mounted) return;
+
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (_) => seen ? const HomeScreen() : const OnboardingScreen(),
+        ),
+      );
+    });
   }
 
   @override
@@ -56,33 +47,30 @@ class _SplashScreenState extends State<SplashScreen>
     return Scaffold(
       backgroundColor: kWhiteColor,
       body: Center(
-        child: FadeTransition(
-          opacity: _fadeAnimation,
+        child: AnimatedOpacity(
+          duration: const Duration(seconds: 2),
+          opacity: opacity,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              ScaleTransition(
-                scale: _scaleAnimation,
-                child: Image.asset(
-                  "assets/images/splach/splach.png",
-                  width: 220,
-                ),
-              ),
+              Image.asset("assets/images/splach/splach.png", width: 350),
 
-              Gap(30),
+              const SizedBox(height: 20),
+
               Text(
                 "Cashy",
                 style: TextStyle(
-                  fontSize: 32,
+                  fontSize: 30,
                   fontWeight: FontWeight.bold,
                   color: kPrimaryOrange,
                 ),
               ),
 
-              Gap(10),
+              const SizedBox(height: 8),
+
               Text(
                 "Track. Save. Grow.",
-                style: TextStyle(fontSize: 16, color: kTextSecondary),
+                style: TextStyle(color: kTextSecondary),
               ),
             ],
           ),
