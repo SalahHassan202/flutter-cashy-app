1962729b75dc70b1ffd398a6c947a889df39f049
SalahHassan202 <salahhassan66190@gmail.com>
2026-05-09 00:16:54 +0300

    feat: setup hive database and register finance model

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
+import 'package:cashy_app/models/finance_model.dart';
 import 'package:cashy_app/screens/splach/splach_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Hive.initFlutter();
+
+  Hive.registerAdapter(FinanceModelAdapter());
+
+  await Hive.openBox<FinanceModel>('financeBox');
+  await Hive.openBox('darkModeBox');
+
   runApp(const MyApp());
 }
 
@@ -11,6 +21,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: SplashScreen());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const SplashScreen(),
+    );
   }
 }
